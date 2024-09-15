97063392e2cb934e1271a727e2b22aa78f65fb1c
dnew0101 <dnew0101@uw.edu>
2024-09-14 20:41:26 -0700

    Initialize NextJS project. TODO updated.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/Layout';
+
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>Contact Us</h1>
+      <p>This is the contact page.</p>
+    </Layout>
+  );
+};
+
+export default Contact;
