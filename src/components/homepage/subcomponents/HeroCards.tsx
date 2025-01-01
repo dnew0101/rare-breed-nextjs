bfa44883ca1e3e48a712380abb4712eacd7b705c
dnew0101 <dnew0101@uw.edu>
2025-01-01 02:34:20 -0800

    centered and height adjusted hero section for mobile

--- src/components/homepage/subcomponents/HeroCards.tsx
+++ src/components/homepage/subcomponents/HeroCards.tsx
@@ -13,13 +13,13 @@ type HeroCardsProps = {
 
 export const HeroCards = ({ className, title, buttonText, route }: HeroCardsProps) => {
   return (
-    <Card className={`w-[80vw] h-[30vh] opacity-70 bg-gradient-to-tl from-background to-black
+    <Card className={`w-[80vw] h-[35vh] opacity-70 bg-gradient-to-tl from-background to-black
         sm:w-[38vw] sm:h-[43vh] 
         lg:w-[27vw] 
         xl:w-[25vw] 
         ${className}`}>
-        <CardHeader className='mt-2 pb-2'>
-            <h1 className='text-3xl sm:text-4xl font-thin text-center' style={{ fontFamily: 'Montserrat, sans-serif' }}>
+        <CardHeader className='mt-1 pb-1'>
+            <h1 className='text-2xl sm:text-4xl font-thin text-center' style={{ fontFamily: 'Montserrat, sans-serif' }}>
                 {title}
             </h1>
         </CardHeader>
@@ -31,7 +31,7 @@ export const HeroCards = ({ className, title, buttonText, route }: HeroCardsProp
 
         <CardFooter className='flex justify-center h-full'>
             <Button
-                className="bg-background w-[70%] rounded-lg p-5 mb-2
+                className="bg-background w-[70%] rounded-lg pt-6 pb-6 pl-2 pr-2 mb-1
                     sm:self-end
                     md:rounded-full"
                 variant='bordered'>
